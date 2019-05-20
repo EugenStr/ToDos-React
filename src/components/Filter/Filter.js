5eacfd8bb19843a70992e30e70c648b2763a923d
Eugen Str <asng99bk@yandex.ru>
2019-05-20 22:42:11 +0400

    fixed Filter functions

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -4,26 +4,16 @@ import './Filter.sass'
 const Filter = ({toogleDone, toogleActive, toogleAll}) => {
   const [toogle, setToogle] = useState(0)
 
-  const onToogleAll = () => {
-    toogleAll();
-    setToogle(0)
-  }
-
-  const onToogleActive = () => {
-    toogleActive();
-    setToogle(1)
-  }
-
-  const onToogleDone = () => {
-    toogleDone();
-    setToogle(2)
+  const onToogle = (func, toogleIndex) => {
+    func();
+    setToogle(toogleIndex)
   }
 
   return (
     <div className="filter-wrapper">
-      <button className={toogle === 0 ? 'filter-btn filter-btn__all active' : 'filter-btn filter-btn__all' } onClick={onToogleAll}>All</button>
-      <button className={toogle === 1 ? 'filter-btn filter-btn__active active' : 'filter-btn filter-btn__active' } onClick={onToogleActive}>Active</button>
-      <button className={toogle === 2 ? 'filter-btn filter-btn__done active' : 'filter-btn filter-btn__done' } onClick={onToogleDone}>Done</button>
+      <button className={toogle === 0 ? 'filter-btn filter-btn__all active' : 'filter-btn filter-btn__all' } onClick={() => onToogle(toogleAll, 0)}>All</button>
+      <button className={toogle === 1 ? 'filter-btn filter-btn__active active' : 'filter-btn filter-btn__active' } onClick={() => onToogle(toogleActive, 1)}>Active</button>
+      <button className={toogle === 2 ? 'filter-btn filter-btn__done active' : 'filter-btn filter-btn__done' } onClick={() => onToogle(toogleDone, 2)}>Done</button>
     </div>
   )
 }
